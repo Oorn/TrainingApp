eb7baf448f0a1968ebe79c749e71d2febde77666
Oorn <pversnp@gmail.com>
2024-01-16 09:12:49 +0400

    Credentials service tests

---
+++ src/test/java/org/example/service/impl/CredentialsServiceImplTest.java
@@ -0,0 +1,80 @@
+package org.example.service.impl;
+
+
+import org.example.domain_entities.User;
+import org.example.repository.UserRepository;
+import org.example.requests_responses.user.UpdateCredentialsRequest;
+import org.example.service.CredentialsServiceUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CredentialsServiceImplTest {
+
+    @Mock
+    private CredentialsServiceUtils credentialsServiceUtils;
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    CredentialsServiceImpl credentialsService;
+
+    @Test
+    void validateUsernamePassword() {
+        String username = "username", passwordRight = "passwordRight", passwordWrong = "passwordWrong";
+        User user = User.builder().build();
+
+        when(userRepository.get(username)).thenReturn(Optional.of(user));
+        when(credentialsServiceUtils.validateUserPassword(user,passwordRight)).thenReturn(true);
+        when(credentialsServiceUtils.validateUserPassword(user,passwordWrong)).thenReturn(false);
+
+        assert credentialsService.validateUsernamePassword(username, passwordRight);
+        assert !credentialsService.validateUsernamePassword(username, passwordWrong);
+        verify(credentialsServiceUtils, times(2)).validateUserPassword(eq(user), anyString());
+
+    }
+
+    @Test
+    void updateCredentials() {
+        String username = "username", passwordOld = "passwordOld", passwordNew = "passwordNew";
+        UpdateCredentialsRequest request = UpdateCredentialsRequest.builder()
+                .oldPassword(passwordOld)
+                .newPassword(passwordNew)
+                .build();
+        User user = User.builder().build();
+
+        when(userRepository.get(username)).thenReturn(Optional.of(user));
+        when(credentialsServiceUtils.validateUserPassword(user,passwordOld)).thenReturn(true);
+        when(credentialsServiceUtils.validatePasswordRequirements(passwordNew)).thenReturn(true);
+
+        assert credentialsService.updateCredentials(username, request);
+        verify(credentialsServiceUtils, times(1)).setUserPassword(user, passwordNew);
+    }
+
+    @Test
+    void generateUsername() {
+        String firstName = "first", lastName = "last", expectedUsername = "first.last3";
+        List<String> existingPrefixUsernames = List.of("first.last", "first.last0", "first.last1", "first.last2");
+        String candidateUsername = firstName + "." + lastName;
+
+        List<User> existingPrefixUsers = existingPrefixUsernames.stream()
+                .map(s-> User.builder()
+                        .userName(s)
+                        .build())
+                .collect(Collectors.toList());
+        when(userRepository.getAllByPrefix(candidateUsername)).thenReturn(existingPrefixUsers);
+
+        assertEquals(expectedUsername, credentialsService.generateUsername(firstName,lastName));
+        verify(userRepository, times(1)).getAllByPrefix(candidateUsername);
+    }
+}
